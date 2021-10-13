c9d05fd66d7d5d0a5eeccc9ed97cf5fc86200c0f
AlyeadaSloh <89851311+AlyeadaSloh@users.noreply.github.com>
2021-10-13 12:26:08 -0400

    Ive changed description and nav bar and other components

--- src/components/descriptions.js
+++ src/components/descriptions.js
@@ -16,12 +16,7 @@ function Descriptions() {
         </div>
         <div className="col">
           <h5 className="title-text">Real Dogs</h5>
-          <p className="sub-text">
-            Dogs are man's best friend. But dogs can also be dog's best friend.
-            Maybe even bester than man's best friend. On furbook, you can find
-            millions of other dogs who can be a loyal companion, trusted
-            confidant, and reliable wrestling partner
-          </p>
+          <p className="sub-text">Real dogs are loyal.</p>
         </div>
         <div className="col">
           <h5 className="title-text">Real Friends</h5>
